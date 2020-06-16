ebb8994918cdcb58010c39246b6d7426ca2f36b5
Damian Olivier <dolivier@keetup.com>
2020-06-16 19:13:06 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NextstepsComponent } from './body/nextsteps/nextsteps.component';
+import { ListadoclientesComponent } from './body/listadoclientes/listadoclientes.component';
+import { ListadosucursalesComponent } from './body/listadosucursales/listadosucursales.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NextstepsComponent },
+  { path: 'clientes', component: ListadoclientesComponent },
+  { path: 'sucursales', component: ListadosucursalesComponent },
+  { path: '**', component: NextstepsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
